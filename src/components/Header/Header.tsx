9f2f67bbec9af6be16909499c53f6e79b76a80fc
omarknows <omarknows98@gmail.com>
2023-08-23 21:04:35 +0300

    Header component design

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,30 @@
 import './Header.css';
+import logo from '../../assets/logo.svg';
+import arrow from '../../assets/icon-arrow-light.svg';
 
 const Header = () => {
-	return <div>Header</div>;
+	return (
+		<div className='header'>
+			<div className='flex-box '>
+				<img className='logo' src={logo} alt='logo' />
+				<div className='header-item'>
+					Product
+					<img src={arrow} />
+				</div>
+				<div className='header-item'>
+					Company <img src={arrow} />
+				</div>
+				<div className='header-item'>
+					Connect
+					<img src={arrow} />
+				</div>
+			</div>
+			<div className='flex-box'>
+				<div className='login'>Login</div>
+				<div className='primary-btn'>Sign Up</div>
+			</div>
+		</div>
+	);
 };
 
 export default Header;
