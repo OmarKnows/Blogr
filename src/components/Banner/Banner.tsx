39812303ac40184f29d781f3e419addf3e57f351
omarknows <omarknows98@gmail.com>
2023-08-23 19:15:58 +0300

    Banner component (bg img needs tweaking)

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,5 +1,22 @@
+import background from '../../assets/bg-pattern-intro-desktop.svg';
+import './Banner.css';
+import '../../App.css';
 const Banner = () => {
-	return <div>Banner</div>;
+	return (
+		<div>
+			<div className='container'>
+				{/* <img className='background' src={background} alt='background' /> */}
+				<div className='banner-items'>
+					<div className='title'>A modern publishing platform</div>
+					<div className='sub-title'> Grow your audience and build your online brand</div>
+					<div className='flex-box'>
+						<div className='primary-btn'>Start for Free</div>
+						<div className='secondary-btn'>Learn More</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Banner;
