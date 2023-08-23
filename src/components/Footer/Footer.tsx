ed2290cf6c35276e9b4ddfacde2f45bb03c95779
omarknows <omarknows98@gmail.com>
2023-08-23 21:57:28 +0300

    Footer design complete

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,25 @@
+import './Footer.css';
+import logo from '../../assets/logo.svg';
+
 const Footer = () => {
-	return <div>Footer</div>;
+	return (
+		<div className='footer'>
+			<img className='logo' src={logo} alt='logo' />
+			<div className='footer-title'>Product</div>
+			<div className='footer-title'>Company</div>
+			<div className='footer-title'>Connect</div>
+			<div className='footer-item'>Pricing</div>
+			<div className='footer-item'>Team</div>
+			<div className='footer-item'>Newsletter</div>
+			<div className='footer-item'>Marketplace</div>
+			<div className='footer-item'>Blog</div>
+			<div className='footer-item'>Linkedin</div>
+			<div className='footer-item'>Features</div>
+			<div className='footer-item'>Careers</div>
+			<div className='footer-item'></div>
+			<div className='footer-item'>Integrations</div>
+		</div>
+	);
 };
 
 export default Footer;
